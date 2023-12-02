800c81d69b41b5e305e748f178b5b7073713f237
riyasba <151115891+riyastechy@users.noreply.github.com>
2023-12-02 18:55:59 +0530

    a

--- lib/app/modules/screens/appprofile/profile/views/profile_view.dart
+++ lib/app/modules/screens/appprofile/profile/views/profile_view.dart
@@ -12,118 +12,121 @@ class ProfileView extends GetView<ProfileController> {
   const ProfileView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.grey[300],
-          leading: InkWell(
-              onTap: () {
-                Get.back();
-              },
-              child: Icon(Icons.arrow_back)),
-          title: Text(
-            'ProfileView',
-            style: appbarfont,
-          ),
-          actions: [
-            InkWell(
-              onTap: () {
-                Get.to(EditprofileView());
-              },
-              child: Padding(
-                padding: const EdgeInsets.only(right: 10),
-                child: Text(
-                  'Edit',
-                  style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 17,
-                      color: Colors.blue),
+    return Container(
+      color: kwhite,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.grey[300],
+            leading: InkWell(
+                onTap: () {
+                  Get.back();
+                },
+                child: Icon(Icons.arrow_back)),
+            title: Text(
+              'ProfileView',
+              style: appbarfont,
+            ),
+            actions: [
+              InkWell(
+                onTap: () {
+                  Get.to(EditprofileView());
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: Text(
+                    'Edit',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 17,
+                        color: Colors.blue),
+                  ),
                 ),
-              ),
-            )
-          ],
-          centerTitle: true,
-        ),
-        body: Center(
-          child: ListView(
-            shrinkWrap: true,
-            physics: BouncingScrollPhysics(),
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 1),
-                        child: Container(
-                            height: 100,
-                            width: 100,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(50)),
-                            child: Image.asset(
-                              "assets/images/fi_2102647.png",
-                            )
-
-                            //  profileController.isLoading.isTrue
-                            //     ? const Center(
-                            //         child: CircularProgressIndicator(),
-                            //       )
-                            // : profileController
-                            //         .otherUserProfileData.isEmpty
-                            //     ? Container()
-                            //     : ClipRRect(
-                            //         borderRadius:
-                            //             BorderRadius.circular(50),
-                            //         child: profileController
-                            //                     .otherUserProfileData
-                            //                     .first
-                            //                     .user
-                            //                     .profilePicture ==
-                            //                 null
-
-                            //             ?
-                            //              Image.asset(
-                            //                 "assets/icons/profil_img.jpeg",
-                            //               )
-
-                            //             : Image.network(
-                            //                 profileController
-                            //                     .otherUserProfileData
-                            //                     .first
-                            //                     .user
-                            //                     .profilePicture,
-                            //               ),
-                            //       ),
-                            ),
-                      ),
-                    ],
-                  ),
-                  ksizedbox20,
-                  FormfieldView(
-                    text: 'Riyas',
-                    labeltext: 'Name',
-                  ),
-                  FormfieldView(
-                    text: '23',
-                    labeltext: 'Age',
-                  ),
-                  FormfieldView(
-                    text: 'Male',
-                    labeltext: 'Gender',
-                  ),
-                  FormfieldView(
-                    text: '9633749714',
-                    labeltext: 'Mobile Number',
-                  ),
-                  FormfieldView(
-                    text: 'riyasklb89@gmail.com',
-                    labeltext: 'Email ID',
-                  ),
-                ],
-              ),
+              )
             ],
+            centerTitle: true,
+          ),
+          body: Center(
+            child: ListView(
+              shrinkWrap: true,
+              physics: BouncingScrollPhysics(),
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 1),
+                          child: Container(
+                              height: 100,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50)),
+                              child: Image.asset(
+                                "assets/images/fi_2102647.png",
+                              )
+
+                              //  profileController.isLoading.isTrue
+                              //     ? const Center(
+                              //         child: CircularProgressIndicator(),
+                              //       )
+                              // : profileController
+                              //         .otherUserProfileData.isEmpty
+                              //     ? Container()
+                              //     : ClipRRect(
+                              //         borderRadius:
+                              //             BorderRadius.circular(50),
+                              //         child: profileController
+                              //                     .otherUserProfileData
+                              //                     .first
+                              //                     .user
+                              //                     .profilePicture ==
+                              //                 null
+
+                              //             ?
+                              //              Image.asset(
+                              //                 "assets/icons/profil_img.jpeg",
+                              //               )
+
+                              //             : Image.network(
+                              //                 profileController
+                              //                     .otherUserProfileData
+                              //                     .first
+                              //                     .user
+                              //                     .profilePicture,
+                              //               ),
+                              //       ),
+                              ),
+                        ),
+                      ],
+                    ),
+                    ksizedbox20,
+                    FormfieldView(
+                      text: 'Riyas',
+                      labeltext: 'Name',
+                    ),
+                    FormfieldView(
+                      text: '23',
+                      labeltext: 'Age',
+                    ),
+                    FormfieldView(
+                      text: 'Male',
+                      labeltext: 'Gender',
+                    ),
+                    FormfieldView(
+                      text: '9633749714',
+                      labeltext: 'Mobile Number',
+                    ),
+                    FormfieldView(
+                      text: 'riyasklb89@gmail.com',
+                      labeltext: 'Email ID',
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/app/modules/screens/bottumnavigation/views/bottumnavigation_view.dart
+++ lib/app/modules/screens/bottumnavigation/views/bottumnavigation_view.dart
@@ -11,7 +11,7 @@ import '../controllers/bottumnavigation_controller.dart';
 
 class BottumnavigationView extends GetView<BottumnavigationController> {
   const BottumnavigationView({Key? key}) : super(key: key);
-    back(BuildContext context) {
+  back(BuildContext context) {
     showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -38,11 +38,8 @@ class BottumnavigationView extends GetView<BottumnavigationController> {
               ),
             ),
             actions: [
-              
               Row(
-
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
-
                 children: [
                   InkWell(
                     onTap: () {
@@ -85,48 +82,49 @@ class BottumnavigationView extends GetView<BottumnavigationController> {
           );
         });
   }
-  
+
   @override
   Widget build(BuildContext context) {
-
-
-      
     return WillPopScope(
-        onWillPop: () {
+      onWillPop: () {
         return back(context);
       },
       child: Scaffold(
         body: Obx(() => controller.screens[controller.currentIndex]),
-        bottomNavigationBar: Obx(()=> AnimatedBottomNavigationBar.builder(
-          backgroundColor: kred,
-          itemCount: controller.iconsList.length,
-         tabBuilder: (int index, bool isActive) {
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: 40,
-                width: 40,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color:isActive? Colors.white : Colors.transparent
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SvgPicture.asset(controller.iconsList[index], fit: BoxFit.contain,color: isActive ? kred : Colors.white,),
-                  ],
-                )),
-            ],
-          );
-         },
-          activeIndex: controller.currentIndex,
-          leftCornerRadius: 15,
-          rightCornerRadius: 15,
-           gapLocation: GapLocation.none,
-          onTap: (value)=> controller.setIndex(value),
-          //other params
-           ),
+        bottomNavigationBar: Obx(
+          () => AnimatedBottomNavigationBar.builder(
+            backgroundColor: kred,
+            itemCount: controller.iconsList.length,
+            tabBuilder: (int index, bool isActive) {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: isActive ? Colors.white : Colors.transparent),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset(
+                            controller.iconsList[index],
+                            fit: BoxFit.contain,
+                            color: isActive ? kred : Colors.white,
+                          ),
+                        ],
+                      )),
+                ],
+              );
+            },
+            activeIndex: controller.currentIndex,
+            leftCornerRadius: 15,
+            rightCornerRadius: 15,
+            gapLocation: GapLocation.none,
+            onTap: (value) => controller.setIndex(value),
+            //other params
+          ),
         ),
       ),
     );
